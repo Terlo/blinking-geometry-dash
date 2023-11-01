cef75f075fee0753eaa860a10fa9ba0d48b0a8fa
Terlo Akintola <terloakintola@gmail.com>
2023-11-01 19:57:17 +0000

    facial detection working

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import cv2
 import numpy as np
+import dlib
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+
+
+
+detector = dlib.get_frontal_face_detector()
 
 while True:
     _ , frame = cap.read()
+
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces  = detector (grey)
+
+    for face in faces:
+        x,y = face.left(), face.top()
+        x1,y1 = face.right(), face.bottom()
+        cv2.rectangle(frame,(x,y),(x1,y1),(0,0,255),2)
 
     cv2.imshow("Frame",frame)
 
     key = cv2.waitKey(1)
     if key ==27:
         break
+
+cap.release()
